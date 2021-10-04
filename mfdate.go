2f06d9f837fc0f58b0f12918c7d2bd9c29ffeaf1
Kirill Zhuchkov <643378+r0busta@users.noreply.github.com>
2021-10-04 09:27:47 +0200

    Update mfdate.go

--- mfdate.go
+++ mfdate.go
@@ -20,10 +20,10 @@ func NewMfDate() MfDate {
 }
 
 func NewMfDateFromTime(t time.Time) MfDate {
-	d := &MfDate{}
+	d := MfDate{}
 	d.initialize(t)
 
-	return *d
+	return d
 }
 
 func NewMfDateFromDate(year int, month int, day int) MfDate {
@@ -40,49 +40,49 @@ func trimToDays(t time.Time) *time.Time {
 	return &res
 }
 
-func (d *MfDate) IsZero() bool {
+func (d MfDate) IsZero() bool {
 	return d.base == nil || d.base.IsZero()
 }
 
-func (d *MfDate) Year() int {
+func (d MfDate) Year() int {
 	return d.base.Year()
 }
 
-func (d *MfDate) Month() int {
+func (d MfDate) Month() int {
 	return int(d.base.Month())
 }
 
-func (d *MfDate) Day() int {
+func (d MfDate) Day() int {
 	return d.base.Day()
 }
 
-func (d *MfDate) Time() time.Time {
+func (d MfDate) Time() time.Time {
 	return *d.base
 }
 
-func (d *MfDate) After(arg MfDate) bool {
+func (d MfDate) After(arg MfDate) bool {
 	return d.base.After(arg.Time())
 }
 
-func (d *MfDate) Before(arg MfDate) bool {
+func (d MfDate) Before(arg MfDate) bool {
 	return d.base.Before(arg.Time())
 }
 
-func (d *MfDate) Equals(arg MfDate) bool {
+func (d MfDate) Equals(arg MfDate) bool {
 	return d.base.Equal(arg.Time())
 }
 
-func (d *MfDate) AddMonthResetDay(mm int) MfDate {
+func (d MfDate) AddMonthResetDay(mm int) MfDate {
 	t := time.Date(d.Year(), time.Month(d.Month()+mm), 1, 0, 0, 0, 0, time.UTC)
 
 	return NewMfDateFromTime(t)
 }
 
-func (d *MfDate) YearMonthEqual(arg MfDate) bool {
+func (d MfDate) YearMonthEqual(arg MfDate) bool {
 	return d.Year() == arg.Year() && d.Month() == arg.Month()
 }
 
-func (d *MfDate) DayBeforeOrEquals(arg MfDate) bool {
+func (d MfDate) DayBeforeOrEquals(arg MfDate) bool {
 	if d.YearMonthEqual(arg) {
 		return true
 	}
@@ -90,7 +90,7 @@ func (d *MfDate) DayBeforeOrEquals(arg MfDate) bool {
 	return d.DayBefore(arg)
 }
 
-func (d *MfDate) DayBefore(arg MfDate) bool {
+func (d MfDate) DayBefore(arg MfDate) bool {
 	if d.Year() < arg.Year() {
 		return true
 	}
